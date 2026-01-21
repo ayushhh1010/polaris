b42c0cc6ded7ec3b450017811c9b04cbe6b6b50f
Ayush Kumar <ayushsingh36582@gmail.com>
2026-01-22 00:06:00 +0530

    04: background jobs

---
+++ src/app/api/demo/blocking/route.ts
@@ -0,0 +1,13 @@
+//localhost:3000/api/demo/blocking
+
+import { generateText } from "ai";
+import { google } from "@ai-sdk/google";
+
+
+export async function POST(){
+    const response = await generateText({
+        model: google("gemini-2.5-flash"),
+        prompt: "Write how do I become a billionaire?",
+    });
+    return Response.json({ response });
+}
