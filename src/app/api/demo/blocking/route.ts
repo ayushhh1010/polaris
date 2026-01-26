f53267bccfd4455871d3eaf733510d51d145a3be
Ayush Kumar <ayushsingh36582@gmail.com>
2026-01-26 20:52:06 +0530

    06: Error Tracking

--- src/app/api/demo/blocking/route.ts
+++ src/app/api/demo/blocking/route.ts
@@ -8,6 +8,11 @@ export async function POST(){
     const response = await generateText({
         model: google("gemini-2.5-flash"),
         prompt: "Write how do I become a billionaire?",
+        experimental_telemetry: {
+            isEnabled: true,
+            recordInputs: true,
+            recordOutputs: true,
+        },
     });
     return Response.json({ response });
 }
